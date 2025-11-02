67acfb5b47393199c37a719c916d7fe94d8716c4
rafaelcoelho1409 <rafaelcoelho1409@hotmail.com>
2025-11-01 21:45:11 -0300

    ArgoCD finalizer added

--- mirror_on_master_cluster.sh
+++ mirror_on_master_cluster.sh
@@ -2,3 +2,6 @@
 ~/K3D/add-port.sh 9092 30001 loadbalancer "Kafka"
 ~/K3D/add-port.sh 5001 30002 loadbalancer "MLflow"
 ~/K3D/add-port.sh 8501 30003 loadbalancer "Streamlit"
+
+#Check later if these commands are really necessary, 
+#and if these ports are naturally available when ArgoCD deploys each deployment and service
