f1be6b26db60df4abfebb8debd702419068cae32
rafaelcoelho1409 <rafaelcoelho1409@hotmail.com>
2025-11-05 14:04:55 -0300

    Updating

--- mirror_on_master_cluster.sh
+++ mirror_on_master_cluster.sh
@@ -1,7 +1,4 @@
-~/K3D/add-port.sh 8000 30000 loadbalancer "FastAPI"
-~/K3D/add-port.sh 9092 30001 loadbalancer "Kafka"
-~/K3D/add-port.sh 5001 30002 loadbalancer "MLflow"
-~/K3D/add-port.sh 8501 30003 loadbalancer "Streamlit"
-
-#Check later if these commands are really necessary, 
-#and if these ports are naturally available when ArgoCD deploys each deployment and service
+~/K3D/bash/add-port.sh 8000 30000 loadbalancer "FastAPI"
+~/K3D/bash/add-port.sh 9092 30001 loadbalancer "Kafka"
+~/K3D/bash/add-port.sh 5001 30002 loadbalancer "MLflow"
+~/K3D/bash/add-port.sh 8501 30003 loadbalancer "Streamlit"
